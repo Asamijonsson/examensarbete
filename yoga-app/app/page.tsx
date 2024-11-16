a8f9a6d51cb8449a21a3df292acaaeff3396d7a7
Asami <90178926+Asamijonsson@users.noreply.github.com>
2024-11-16 13:53:14 +0100

    innitial commit

--- yoga-app/app/page.tsx
+++ yoga-app/app/page.tsx
@@ -1,10 +1,12 @@
 import styles from "./page.module.css";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <p>welcome!</p>
+        <p>welcome to Yo! Yoga!</p>
+        <Image src="./image/adam-vradenburg-GA09PKfRIQY-unsplash.jpg" alt="" />
       </main>
     </div>
   );
